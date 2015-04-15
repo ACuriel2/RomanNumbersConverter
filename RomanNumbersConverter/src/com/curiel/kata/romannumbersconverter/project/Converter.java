86b7f3fd5c76132116f4d4e446ed8b763b4c2ce5
ACuriel2 <antonio.curiel@tralix.com>
2015-04-15 11:15:27 -0500

    Green - Five to V Test
    The if statement change to switch statement

--- RomanNumbersConverter/src/com/curiel/kata/romannumbersconverter/project/Converter.java
+++ RomanNumbersConverter/src/com/curiel/kata/romannumbersconverter/project/Converter.java
@@ -2,8 +2,18 @@ package com.curiel.kata.romannumbersconverter.project;
 
 public class Converter {
 
-	public String getRoman(int arabicNumber) {
-		return arabicNumber == 1 ? "I" : "";
+	public String getRoman(int arabicNumber) {		
+		switch (arabicNumber) {
+		case 0:
+			return "";			
+		case 1:
+			return "I";
+		case 5:
+			return "V";		
+		default:
+			break;
+		}
+		return "";
 	}
 
 }
